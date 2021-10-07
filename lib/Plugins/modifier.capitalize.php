63c44f6adc90bba1a4e8ae1ddcc19ed1316f5f4e
Anton <info@ensostudio.ru>
2021-10-07 08:27:30 +0300

    Update modifier.capitalize.php

--- lib/Plugins/modifier.capitalize.php
+++ lib/Plugins/modifier.capitalize.php
@@ -14,31 +14,30 @@
  * Name:     capitalize<br>
  * Purpose:  capitalize words in the string
  *
- * @param string $string    string to capitalize
- * @param boolean $uc_digits also capitalize "x123" to "X123"
- * @param boolean $lc_rest   capitalize first letters, lowercase all following letters "aAa" to "Aaa"
+ * @param string $string string to capitalize
+ * @param bool $uc_digits also capitalize "x123" to "X123"
  * @return string capitalized string
- *
  */
-
 function fenom_modifier_capitalize($string, $uc_digits = false)
 {
     fenom_modifier_capitalize_ucfirst(null, $uc_digits);
-    return preg_replace_callback('!\'?\b\w(\w|\')*\b!', 'fenome_modifier_capitalize_ucfirst', $string);
+    return preg_replace_callback('!\'?\b\w(\w|\')*\b!', 'fenom_modifier_capitalize_ucfirst', $string);
 }
 
+/**
+ * @param string|null $string string to capitalize
+ * @param bool|null $uc_digits also capitalize "x123" to "X123"
+ * @return string
+ */
 function fenom_modifier_capitalize_ucfirst($string, $uc_digits = null)
 {
     static $_uc_digits = false;
-    if (isset($uc_digits)) {
+    if ($uc_digits !== null) {
         $_uc_digits = $uc_digits;
-        return;
+        return '';
     }
-    if (substr($string[0], 0, 1) != '\'' && !preg_match('!\d!', $string[0]) || $_uc_digits) {
+    if ($_uc_digits || substr($string[0], 0, 1) !== "'" && !preg_match('!\d!', $string[0])) {
         return ucfirst($string[0]);
-    } else {
-        return $string[0];
     }
+    return $string[0];
 }
-
-?>
