dfcd93979589c82489aaf974b221995d9327af96
Anton <info@ensostudio.ru>
2021-10-07 09:03:13 +0300

    Update Traitor.php
    Исправил пространство имен
    убрал передачу значение по ссылке по сообращениям безопасности + используется filter_input_array
    добавил глобальную переменную `env`(`$_ENV`)

--- lib/Fenom/Traitor/Traitor.php
+++ lib/Fenom/Traitor/Traitor.php
@@ -1,25 +1,27 @@
 <?php
 
-namespace Fenom;
-
+namespace Fenom\Traitor;
 
 class Traitor extends \ArrayObject
 {
-
     public function __construct()
     {
-        $this["version"] = \Fenom::VERSION;
-        $this["ldelim"] = '{';
-        $this["rdelim"] = '}';
-        $this['get'] = & $_GET;
-        $this['post'] = & $_POST;
-        $this['request'] = & $_REQUEST;
-        $this['cookie'] = & $_COOKIE;
-        $this['files'] = & $_FILES;
-        $this['server'] = & $_SERVER;
-        $this['session'] = & $_SESSION;
-        $this['globals'] = & $GLOBALS;
-        $this['now'] = new Traitor\Now();
-        $this['const'] = new Traitor\Constant();
+        $data = [
+            'version' => \Fenom::VERSION,
+            'ldelim' => '{',
+            'rdelim' => '}',
+            'get' => filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING),
+            'post' => filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING),
+            'request' => filter_input_array(INPUT_REQUEST, FILTER_SANITIZE_STRING),
+            'cookie' => filter_input_array(INPUT_COOKIE, FILTER_SANITIZE_STRING),
+            'files' => isset($_FILES) ? $_FILES : [],
+            'server' => filter_input_array(INPUT_SERVER, FILTER_SANITIZE_STRING),
+            'env' => filter_input_array(INPUT_ENV, FILTER_SANITIZE_STRING),
+            'session' => isset($_SESSION) ? $_SESSION : [],
+            'globals' => isset($GLOBALS) ? $GLOBALS : [],
+            'now' => new Now(),
+            'const' => new Constant(),
+        ];
+        parent::__construct($data, self::ARRAY_AS_PROPS);
     }
 }
